0199a80b228f5890c5ce826af4c95d28cc8fd714
eldani13 <ll-luis123@hotmail.com>
2025-05-27 18:53:31 -0500

    Section nueva

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import DiamondShapeSection from "@/components/Featured/DiamondShapeSection";
 import FeaturedCollectionSection from "@/components/Featured/FeaturedCollectionSection";
 import HeroSection from "@/components/Hero/HeroSection";
 import Categories from "@/components/Other/Categories";
+import Information from "@/components/Other/Information";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <Categories/>
       <FeaturedCollectionSection/>
       <DiamondShapeSection/>
       <BrilliantMomentsCollection/>
-      <Categories/>
+      <Information/>
     </div>
   );
 }
---
+++ src/components/Other/Information.tsx
@@ -0,0 +1,81 @@
+export default function Information() {
+  return (
+    <section className="bg-gradient-to-b from-white to-gray-50 py-20 px-6 md:px-12 lg:px-24 text-gray-800">
+      <div className="max-w-7xl mx-auto">
+        <h2 className="text-4xl md:text-5xl font-extrabold text-center text-[#3c2205] mb-2 leading-tight">
+          Elegance that Tells Stories
+        </h2>
+        <p className="text-center text-lg  text-gray-600 max-w-3xl mx-auto mb-16">
+          From necklaces and rings to bracelets and earrings, each piece is
+          designed to capture the essence of timeless elegance with a modern
+          twist.{" "}
+        </p>
+
+        <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+          <div className="p-8 rounded-lg ">
+            <h3 className="text-2xl font-semibold text-[#3c2205] mb-4">
+              Our Historys
+            </h3>
+            <p className="text-gray-700">
+              Since 2010, we've been merging artisanal tradition and
+              contemporary design. Our jewelry reflects passion, detail, and a
+              love of art.{" "}
+            </p>
+          </div>
+
+          <div className="p-8 rounded-lg ">
+            <h3 className="text-2xl font-semibold text-[#3c2205] mb-4">
+              Premium Materials
+            </h3>
+            <p className="text-gray-700">
+              We use 14k and 18k gold, 925 silver, diamonds, and certified
+              gemstones, ensuring luxury, durability, and ethical commitment.{" "}
+            </p>
+          </div>
+
+          <div className="p-8 rounded-lg ">
+            <h3 className="text-2xl font-semibold text-[#3c2205] mb-4">
+              Exclusive Collections
+            </h3>
+            <p className="text-gray-700">
+              Necklaces, rings, earrings, and bracelets designed for every
+              occasion. Inspired by the natural elegance and organic forms of
+              the modern world.{" "}
+            </p>
+          </div>
+
+          <div className="p-8 rounded-lg ">
+            <h3 className="text-2xl font-semibold text-[#3c2205] mb-4">
+              Our Mission
+            </h3>
+            <p className="text-gray-700">
+              Celebrating individuality through meaningful jewelry. We want
+              everyone to express themselves with pieces that transcend time.{" "}
+            </p>
+          </div>
+
+          <div className="p-8 rounded-lg ">
+            <h3 className="text-2xl font-semibold text-[#3c2205] mb-4">
+              Sustainable Commitment{" "}
+            </h3>
+            <p className="text-gray-700">
+              Planet-conscious jewelry: ethical processes, recycled materials,
+              and eco-friendly packaging. Conscious beauty in every detail.{" "}
+            </p>
+          </div>
+
+          <div className="p-8 rounded-lg ">
+            <h3 className="text-2xl font-semibold text-[#3c2205] mb-4">
+              Detailed Craftsmanship{" "}
+            </h3>
+            <p className="text-gray-700">
+              Each piece of jewelry is the result of hours of handwork and
+              precision. Expert artisans bring to life pieces that speak for
+              themselves.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
